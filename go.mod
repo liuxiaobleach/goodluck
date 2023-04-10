566d05b59a6141607e518fa0445a05d042ed2ed9
liuxiaobleach <1241368737@qq.com>
2023-04-10 15:27:41 +0800

    add test

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/liuxiaobleach/goodluck
 
 go 1.19
+
+require (
+	github.com/celer-network/goutils v0.1.60 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	gopkg.in/resty.v1 v1.12.0 // indirect
+)
---
+++ ppricetest/main/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"github.com/celer-network/goutils/log"
+	"gopkg.in/resty.v1"
+)
+
+func main() {
+	r := resty.New().
+		SetHostURL("https://pro-api.coinmarketcap.com").
+		SetHeader("Accepts", "application/json").
+		SetHeader("X-CMC_PRO_API_KEY", "")
+
+	//path := "/v2/cryptocurrency/quotes/latest?id=6756"
+	path := "/v2/cryptocurrency/quotes/latest?symbol=ACA"
+	res, err := r.R().
+		SetContext(context.Background()).
+		SetResult(&CmcResp{}).
+		Get(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res.StatusCode() != 200 {
+		log.Fatalf("invalid res code: %d", res.StatusCode())
+	}
+	quotes := res.Result().(*CmcResp).Data
+	log.Infof("%+v", quotes)
+}
+
+type CmcResp struct {
+	Data TokenQuotes
+}
+
+type Quote struct {
+	Price float64
+}
+
+type TokenQuote struct {
+	Quote map[string]Quote
+}
+
+type TokenQuotes map[string][]TokenQuote
